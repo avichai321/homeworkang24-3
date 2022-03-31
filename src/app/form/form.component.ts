972cbc8b57daa409c8da232d0f94e4b89ba835a6
avichai321 <77723644+avichai321@users.noreply.github.com>
2022-03-31 22:45:28 +0300

    31.3.22

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Optional } from '@angular/core';
 import { ansOptions } from './ansOptions';
 import { FormsModule ,NgForm , NgModel,ReactiveFormsModule} from '@angular/forms';
 
+export interface qa{
+
+}
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
@@ -11,42 +14,44 @@ export class FormComponent implements OnInit {
 
   constructor() { 
   }
-  
-  
-  ngOnInit(): void {
-    
-    
-  } 
-  
+  formcal: any = {
+    firstname:"",
+  }
   total:number=0;
   points=ansOptions
   formHidden:boolean=false
   showResult:boolean = true
+  readonly quizResult_key = 'quiz result';
   
-  answerResult(opa :ansOptions){ 
-    switch(opa){
-      case ansOptions.wrong:
-        if ( this.total != 0){
-          this.total += this.points.wrong;
-          console.log(this.total)
-        }
-        break;
-      case ansOptions.Correct:
-      this.total += this.points.Correct;
-      console.log(this.total)
-      break;
-    }
-    
-      
-    
-  }
-   
-  submitfor(form: any){
-    this.formHidden=true;
-    this.showResult=false;
+  
 
-
-  }
+ 
+ 
+handlesave(){
+  localStorage.setItem(this.quizResult_key,JSON.stringify(this.formcal))
+}
+ 
+  submitfor(form1: NgForm){
+  this.formcal = form1
+  for (let index = 1; index <=5; index++) {
+   let qa= "opa" + index
+   console.log(qa)
+   if (this.formcal[qa] === "10"){
+      this.total+= 10      
    }
+   console.log(this.total)
+   this.formHidden=true;
+   this.showResult=false;
+   
+  }   
+   }
+   ngOnInit(): void {
+    let savedResultsJson = localStorage.getItem(this.formcal);
+    if(savedResultsJson != null){
+      this.formcal = JSON.parse(savedResultsJson);
+    
+  } 
+}
+  }
 
 
